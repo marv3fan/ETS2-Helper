8d26674b458d714615837eec5c719f7ec8240733
marv3fan <masonts@gmail.com>
2019-01-04 11:22:32 -0600

    Added serialization.  Currently serializes cities. Also changed from const char* to std::string throughout the project.
    modifications:
    
    .gitignore:  Added an ignore for Cities.dat, which is the file created when we serialize data.
    
    City.cpp: Added Serialize function which takes a pointer to an outfile stream and then serializes the City via name, country.name (rather than country pointer), latitude, longitude, and garagetype
    
    City.h:  converted City.Name to std::string, adjusted City constructor declaration accordingly.  Also added Serialize() function declaration.
    
    Country.h: Converted Country.Name to std::string, adjusted Country constructor declaration accordingly.
    
    ETS2 Helper.depend: Code::Blocks added our new header includes to this file.
    
    main.cpp:  Created global define for CitySaveFile.  Adjusted menu for new options, Populate Cities with Saved Data and Save (without exit)
    	 Removed PopulateCountries() call from menu options.  PopulateCountries() now occurs at the beginning of main() since we shouldn't have to serialize countries.
    	 Adjusted CreateCity() functions to take strings isntead of const char*'s
    	 Added Serialize() method to open a savefile, poll the cities to serialize, and then close the save file;
    	 Added Deserialize() method to iterate through the savefile after opening, calling CreateCity() with the data it gets;
    
    main.h: Changed declarations for CreateCity() methods to take strings instead of const char*.  Added declarations for Serialize() and Deserialize()

--- City.cpp
+++ City.cpp
@@ -1,6 +1,18 @@
 #include "City.h"
 #include "Garage.h"
+#include <fstream>
+#include <cstring>
 
-Garage::GarageType City::GarageType(){
+Garage::GarageType City::GarageType()
+{
     return garageType;
+}
+
+void City::Serialize(ofstream& savefile)
+{
+    savefile << Name << '\n';
+    savefile << CityCountry.Name << '\n';
+    savefile << Latitude << '\n';
+    savefile << Longitude << '\n';
+    savefile << garageType << '\n';
 }
--- City.h
+++ City.h
@@ -1,8 +1,13 @@
 #ifndef CITY_H_INCLUDED
 #define CITY_H_INCLUDED
 
+#include <fstream>
+#include <string>
 #include "Country.h"
 #include "Garage.h"
+
+using namespace std;
+
 class Country;
 
 class City
@@ -10,16 +15,17 @@ class City
 private:
     Garage::GarageType garageType;
 public:
-    const char* Name;
+    string Name;
     const Country &CityCountry;
     const double Latitude;
     const double Longitude;
 
-   City(const char* CityName, Country &InCountry, double Lat, double Lon, Garage::GarageType GarageType)
+    City(string CityName, Country &InCountry, double Lat, double Lon, Garage::GarageType GarageType)
         : garageType(GarageType), Name(CityName), CityCountry(InCountry), Latitude(Lat), Longitude(Lon)
     {}
 
     Garage::GarageType GarageType();
+    void Serialize(ofstream&);
 };
 
 #endif // CITY_H_INCLUDED
